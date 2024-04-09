5fa72b467e2186a39a54c5e7cb770b0a9e04db0e
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-04-09 22:02:03 +0530

    db functions

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,15 @@
 const mongoosee = require("mongoose");
+
+mongoosee.connect("mongodb+srv://shashankxrm:iNCORRECT%4045@cluster0.mwm4ybk.mongodb.net/")
+
+const todoSchema = mongoosee.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+
+const todo = mongoosee.model("todos", todoSchema);
+
+module.exports = {
+    todo: todo
+}
--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const { createTodo, updateTodo } = require("./types");
 const app = express();
 app.use(express.json());
 
-app.post("/todo", function(req, res){
+app.post("/todo", async function(req, res){
     const createPayLoad = req.body;
     const parsedPayLoad = createTodo.safeParse(createPayLoad);
     if(!parsedPayLoad.success){
@@ -13,13 +13,22 @@ app.post("/todo", function(req, res){
         return;
     }
     // put it in mongodb
+    await todo.create({
+        title: createPayLoad.title,
+        description: createPayLoad
+        .description,
+        completed: false
+    })
 })
 
-app.get("/todo", function(req, res){
-    
+app.get("/todo",  async function(req, res){
+    const todos = await todo.find({});
+    res.json({
+        todos
+    })
 })
 
-app.put("/completed", function(req, res){
+app.put("/completed", async function(req, res){
     const updatePayLoad = req.body;
     const parsedPayLoad = updateTodo.safeParse(updatePayLoad);
     if(!parsedPayLoad.success){
@@ -28,4 +37,12 @@ app.put("/completed", function(req, res){
         })
         return;
     }
+    await todo.update({
+        _id: req.body._id
+    },{
+        completed: true
+    })
+    res.json({
+        msg: "Todo marked as completed"
+    })
 })
