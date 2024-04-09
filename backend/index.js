404003c6f2fdbad4a3fe28ee5370493b4e457d23
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-04-09 20:43:18 +0530

    added post and completed endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 app.use(express.json());
 
 app.post("/todo", function(req, res){
-
+    const createPayLoad = req.body;
+    const parsedPayLoad = createTodo.safeParse(createPayLoad);
+    if(!parsedPayLoad.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
+    // put it in mongodb
 })
 
 app.get("/todo", function(req, res){
@@ -11,5 +20,12 @@ app.get("/todo", function(req, res){
 })
 
 app.put("/completed", function(req, res){
-    
+    const updatePayLoad = req.body;
+    const parsedPayLoad = updateTodo.safeParse(updatePayLoad);
+    if(!parsedPayLoad.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
 })
