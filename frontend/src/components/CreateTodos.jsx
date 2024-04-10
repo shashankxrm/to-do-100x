e5872b46d7c8dc1c41dd54389fca65e874ca16e6
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-04-11 01:27:40 +0530

    create-todos

---
+++ frontend/src/components/CreateTodos.jsx
@@ -0,0 +1,12 @@
+
+
+function CreateTodos() {
+  return (
+    <div>
+        <input type='text' placeholder='title'></input>
+        <input type='text' placeholder='description'></input>
+    </div>
+  )
+}
+
+export default CreateTodos
